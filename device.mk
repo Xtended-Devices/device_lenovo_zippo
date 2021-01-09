54cc572ec0dc0e97ec8ea227ef50210644078bec
Einar Gednochsson <pete80pro.pb@gmail.com>
2021-01-09 09:16:27 +0100

    Revert "zippo: Add GoogleCameraGo"
    This reverts commit 34278d91ef534731d7ba2d098062dece716c0788.

--- device.mk
+++ device.mk
@@ -126,7 +126,7 @@ PRODUCT_PACKAGES += \
     libgui_vendor \
     libqti-perfd-client \
     libxml2 \
-    GoogleCameraGo
+    Snap
 
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.camera.flash-autofocus.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.camera.flash-autofocus.xml \
